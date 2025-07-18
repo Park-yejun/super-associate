5ce11de3e9e66ad3d38d614e4ab155b78e2cc25b
Park-yejun <pyj24252425@gmail.com>
2025-07-18 10:58:46 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 컨테이너가 시작될 때, 설치된 모든 패키지 목록을 로그에 출력합니다.
-echo "--- Checking installed packages in the final container ---"
+echo "--- Checking installed packages in the virtual environment ---"
 pip freeze
 echo "--------------------------------------------------------"
 
